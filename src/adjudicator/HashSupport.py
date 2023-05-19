89c0688a98d3bba7aea135f920d07fa1a5e996a4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-05-19 09:43:31 +0000

    feature: add `HashSupport.fallback()`

--- src/adjudicator/HashSupport.py
+++ src/adjudicator/HashSupport.py
@@ -11,6 +11,7 @@ class HashSupport:
 
     def __init__(self) -> None:
         self._custom_hashers: dict[type, Hasher] = {}
+        self._fallback_hashers: list[Hasher] = []
 
     def register(self, type_: type, hash_function: Hasher) -> None:
         """
@@ -19,13 +20,30 @@ class HashSupport:
 
         self._custom_hashers[type_] = hash_function
 
+    def fallback(self, hash_funtion: Hasher) -> None:
+        """
+        Register a fallback hash function that will be used if no custom hash function is registered for a given type.
+        The function should either return `NotImplemented` or raise a `NotImplementedError` if it cannot hash the given
+        object.
+        """
+
+        self._fallback_hashers.append(hash_funtion)
+
     def __call__(self, obj: Any) -> int:
         """
         Hash the given object.
         """
 
-        try:
-            hash_func = self._custom_hashers[type(obj)]
-        except KeyError:
-            hash_func = hash
-        return hash_func(obj)
+        hash_func = self._custom_hashers.get(type(obj))
+        if hash_func is not None:
+            return hash_func(obj)
+
+        for hash_func in self._fallback_hashers:
+            try:
+                result = hash_func(obj)
+            except NotImplementedError:
+                pass
+            if result is not NotImplemented:
+                return result
+
+        return hash(obj)
